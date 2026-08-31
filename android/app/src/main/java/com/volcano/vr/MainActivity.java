31bfb042cb6d1849f8812b7578878066b3be4c16
mulrich-ort <marcelo.ulrich@ort.edu.ar>
2026-08-31 11:04:16 -0300

    modif

--- android/app/src/main/java/com/volcano/vr/MainActivity.java
+++ android/app/src/main/java/com/volcano/vr/MainActivity.java
@@ -1,5 +1,22 @@
 package com.volcano.vr;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Habilitar aceleración de hardware y WebGL/WebXR en el WebView
+        if (this.bridge != null && this.bridge.getWebView() != null) {
+            WebSettings settings = this.bridge.getWebView().getSettings();
+            settings.setJavaScriptEnabled(true);
+            settings.setDomStorageEnabled(true);
+            settings.setDatabaseEnabled(true);
+            settings.setAllowFileAccess(true);
+            settings.setAllowContentAccess(true);
+        }
+    }
+}
